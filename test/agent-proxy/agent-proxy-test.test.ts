ca9355df8d9e2ddf69da81fbfb782b843eb0397a
Gregor Martynus <gregor@martynus.net>
2019-08-09 16:19:00 -0700

    test: `octokit.request()`

---
+++ test/agent-proxy/agent-proxy-test.test.ts
@@ -0,0 +1,67 @@
+/*!
+ * Tests are based on work by Nathan Rajlich:
+ * https://github.com/TooTallNate/node-http-proxy-agent/blob/65307ac8fe4e6ce1a2685d21ec4affa4c2a0a30d/test/test.js
+ *
+ * Copyright (c) 2013 Nathan Rajlich <nathan@tootallnate.net>
+ * Released under the MIT license
+ */
+const http = require("http");
+
+const HttpProxy = require("proxy");
+const HttpProxyAgent = require("http-proxy-agent");
+
+import { Octokit } from "../../src";
+
+describe("client proxy", () => {
+  let proxy: any;
+  let proxyUrl: string;
+
+  // start HTTP proxy & http server
+  beforeAll(done => {
+    proxy = HttpProxy();
+    proxy.listen(() => {
+      proxyUrl = "http://localhost:" + proxy.address().port;
+      done();
+    });
+  });
+
+  let server: any;
+  beforeAll(done => {
+    server = http.createServer((request: any, response: any) => {
+      expect(request.method).toEqual("GET");
+      expect(request.url).toEqual("/");
+
+      response.writeHead(200);
+      response.write("ok");
+      response.end();
+    });
+
+    server.listen(0, done);
+  });
+
+  // stop proxy HTTP & http server
+  afterAll(done => {
+    proxy.once("close", () => done());
+    proxy.close();
+  });
+
+  afterAll(done => server.close(done));
+
+  it("options.agent = new HttpProxyAgent(proxyUrl)", async () => {
+    let proxyReceivedRequest;
+
+    proxy.once("request", (request: any) => {
+      expect(request.headers.accept).toBe("application/vnd.github.v3+json");
+      proxyReceivedRequest = true;
+    });
+
+    const octokit = new Octokit({
+      baseUrl: "http://localhost:" + server.address().port,
+      request: { agent: new HttpProxyAgent(proxyUrl) }
+    });
+
+    await octokit.request("/");
+
+    expect(proxyReceivedRequest).toBe(true);
+  });
+});
