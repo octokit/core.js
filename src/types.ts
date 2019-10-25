b839e4605e98271a8990dba588b8b0968ce2c54b
Gregor Martynus <gregor@martynus.net>
2019-10-25 16:04:31 -0700

    refactor: use AuthInterface from @octokit/types

--- src/types.ts
+++ src/types.ts
@@ -5,19 +5,10 @@ import { Octokit } from ".";
 export type RequestParameters = OctokitTypes.RequestParameters;
 
 export type OctokitOptions = {
-  auth?: string | AutenticationHook;
+  auth?: string | OctokitTypes.AuthInterface;
   request?: OctokitTypes.RequestRequestOptions;
   timeZone?: string;
   [option: string]: any;
 };
-
-interface AutenticationHook {
-  (options?: any): any;
-
-  hook: (
-    request: OctokitTypes.RequestInterface,
-    options: OctokitTypes.EndpointOptions
-  ) => ReturnType<OctokitTypes.RequestInterface>;
-}
 
 export type Plugin = (octokit: Octokit, options: OctokitOptions) => void;
