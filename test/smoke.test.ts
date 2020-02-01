5b29b4ad666f35b79d6a72e1270b16644d810622
Gregor Martynus <gregor@martynus.net>
2020-01-31 16:41:29 -0800

    test: Octokit.VERSION

--- test/smoke.test.ts
+++ test/smoke.test.ts
@@ -3,6 +3,7 @@ import { Octokit } from "../src";
 describe("Smoke test", () => {
   it("is a function", () => {
     expect(Octokit).toBeInstanceOf(Function);
+    expect(Octokit.VERSION).toEqual("0.0.0-development");
     expect(() => new Octokit()).not.toThrow();
   });
 });
