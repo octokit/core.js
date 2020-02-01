52d579c5b564b4e0e9c76009ebc40b69ff8d4c95
Gregor Martynus <gregor@martynus.net>
2020-01-31 16:41:34 -0800

    feat: Octokit.VERSION

--- src/index.ts
+++ src/index.ts
@@ -14,6 +14,7 @@ import {
 import { VERSION } from "./version";
 
 export class Octokit {
+  static VERSION = VERSION;
   static defaults<S extends Constructor<any>>(
     this: S,
     defaults: OctokitOptions
