07aa79418f45fd4211907c610696ad0e0d56b1b7
Gregor Martynus <gregor@martynus.net>
2020-05-19 14:40:28 -0700

    fix: send GraphQL requests to the correct URL for GitHub Enterprise Server (#66)

--- test/graphql.test.ts
+++ test/graphql.test.ts
@@ -49,4 +49,44 @@ describe("octokit.graphql()", () => {
 
     expect(result).toStrictEqual(mockResult);
   });
+
+  it("GitHub Enterprise Server usage (with option.baseUrl)", async () => {
+    const mockResult = {
+      organization: {
+        repositories: {
+          totalCount: 123,
+        },
+      },
+    };
+    const mock = fetchMock
+      .sandbox()
+      .postOnce("https://github.acme-inc.com/api/graphql", (url, request) => {
+        const body = JSON.parse(request.body!.toString());
+        expect(body.query).toEqual(query);
+
+        return {
+          data: mockResult,
+        };
+      });
+
+    const octokit = new Octokit({
+      auth: `secret123`,
+      baseUrl: "https://github.acme-inc.com/api/v3",
+      request: {
+        fetch: mock,
+      },
+    });
+
+    const query = `query ($login: String!) {
+      organization(login: $login) {
+        repositories(privacy: PRIVATE) {
+          totalCount
+        }
+      }
+    }`;
+
+    const result = await octokit.graphql(query, { login: "octokit" });
+
+    expect(result).toStrictEqual(mockResult);
+  });
 });
